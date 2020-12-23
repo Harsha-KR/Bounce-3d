e9c2bb2c30b430a1ef325b1653c3066f1d3c7c9d
harsha_kr3 <harsha.kr3@gmail.com>
2020-12-23 20:29:45 +0530

    Working on player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody PlayerRb;
+    [SerializeField]
+    float MovementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
+        {
+            Vector3 newPosition = PlayerRb.position + (Vector3.right * HorizontalInput * MovementSpeed * Time.deltaTime);
+
+            PlayerRb.MovePosition(newPosition);
+        }
+    }
+}
