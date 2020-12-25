256e1d7e75f3573c4ff35e2e2910ddd3a05efa38
harsha_kr3 <harsha.kr3@gmail.com>
2020-12-25 20:22:30 +0530

    Camera is set up. Collectables are set up.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,30 @@ public class Player : MonoBehaviour
 {
     Rigidbody PlayerRb;
     [SerializeField]
+    float RotationSpeed;
+    [SerializeField]
     float MovementSpeed;
+    [SerializeField]
+    float JumpForce;
+    Vector3 m_EulerAngleVelocity;
+    [SerializeField]
+    Material CollectableAfter;
 
-    // Start is called before the first frame update
     void Start()
     {
+        m_EulerAngleVelocity = new Vector3(0, 0, RotationSpeed);
         PlayerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         Movement();
+        
+    }
+
+    private void Update()
+    {
+        Jump();
     }
 
     private void Movement()
@@ -26,9 +38,32 @@ public class Player : MonoBehaviour
 
         if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
         {
+            PlayerRb.angularVelocity = Vector3.zero;
+
             Vector3 newPosition = PlayerRb.position + (Vector3.right * HorizontalInput * MovementSpeed * Time.deltaTime);
 
             PlayerRb.MovePosition(newPosition);
+
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * -HorizontalInput );
+            PlayerRb.MoveRotation(PlayerRb.rotation * deltaRotation);
+        }        
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {            
+            PlayerRb.AddForce(Vector3.up * JumpForce, ForceMode.Force);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Collectable":
+                other.gameObject.GetComponent<MeshRenderer>().material = CollectableAfter;
+                break;
         }
     }
 }
