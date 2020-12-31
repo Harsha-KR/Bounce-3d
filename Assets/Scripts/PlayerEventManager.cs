b8ad347c09187da68c62217965e6d4a07cfc7574
Harshak-r1 <Harsha-kr1@users.noreply.github.com>
2020-12-31 14:40:23 +0530

    Events implemented

---
+++ Assets/Scripts/PlayerEventManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEventManager : MonoBehaviour
+{
+    public delegate void CollectedCollectable(Collider any);
+    public static event CollectedCollectable Collectable;
+    public static event CollectedCollectable Lives;
+    public static event CollectedCollectable Checkpoint;
+    bool isDead;
+
+    public delegate void PlayerDied();
+    public static event PlayerDied Dead;
+    private void Start()
+    {
+        isDead = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Collectable":
+                Collectable?.Invoke(other);
+                break;
+            case "Lives":
+                Lives?.Invoke(other);
+                break;
+            case "Checkpoint":
+                Checkpoint?.Invoke(other);
+                break;
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Enemy":
+                DeathLogic();
+                break;
+        }
+    }
+
+    private void DeathLogic()
+    {
+        if (!isDead)
+        {
+            isDead = true;
+            Destroy(this.gameObject);
+            Dead?.Invoke();          
+        }
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,6 +11,18 @@ public class SpawnManager : MonoBehaviour
 
     Vector3 SpawnPosition;
 
+    private void OnEnable()
+    {
+        PlayerEventManager.Checkpoint += UpdateSpawnPosition;
+        PlayerEventManager.Dead += spawnner;
+        PlayerEventManager.Lives += LivesCollected;
+    }
+    private void OnDisable()
+    {
+        PlayerEventManager.Checkpoint -= UpdateSpawnPosition;
+        PlayerEventManager.Dead -= spawnner;
+        PlayerEventManager.Lives -= LivesCollected;
+    }
     private void Start()
     {
         lives = 4;
@@ -18,11 +30,12 @@ public class SpawnManager : MonoBehaviour
         Instantiate(Player, SpawnPosition,Quaternion.identity);
     }
 
-    public void UpdateSpawnPosition(Vector3 position)
+    private void UpdateSpawnPosition(Collider other)
     {
-        SpawnPosition = position;
+        SpawnPosition = other.transform.position;
+        
     }
-    public void spawnner()
+    private void spawnner()
     {
         lives--;
         if (lives > 0)
@@ -30,9 +43,13 @@ public class SpawnManager : MonoBehaviour
             Instantiate(Player, SpawnPosition, Quaternion.identity);
         }
     }
-    public void PowerUpCollected(Collider PowerUp)
+
+    //since this method has no other function than destroying the object, did not find any reason to create a seperate script. 
+    //If in future it has more functions then think about a seperate script. 
+
+    private void LivesCollected(Collider other)
     {
-        Destroy(PowerUp.gameObject);
+        Destroy(other.gameObject);
         lives++;
     }
 }
